3eae219533f83c273aaa8fd18c1d62c746aa3fc9
jacerobinson21@gmail.com <12394543+JaceRobinson8@users.noreply.github.com>
2023-12-16 22:40:46 -0600

    sol d6c1 and c2

--- days/day_6/challenge_1/common/utils.py
+++ days/day_6/challenge_1/common/utils.py
@@ -1,13 +1,54 @@
 from pathlib import Path
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import logging
+from math import sqrt, floor, ceil
+import re
 
 logger = logging.getLogger(__name__)
 
 
-def parse_input(file_path: Path = Path("./input/input.txt")) -> None:
+@dataclass
+class Race:
+    time: int
+    record_distance: int
+
+    @property
+    def beat_record_count(self) -> int:
+        # the boat problem forms a quadratic equation
+        # The "winning" solutions are the count of integer x values between zeros of the quadratic.
+        x = quadratic_solver(1.0, -1.0 * self.time, float(self.record_distance))
+        # want to be NOT inclusive on the ends, if I didn't need to round, reduce by 1
+        if floor(x[1]) < x[1]:
+            x1 = floor(x[1])
+        else:
+            x1 = floor(x[1]) - 1
+
+        if ceil(x[0]) > x[0]:
+            x0 = ceil(x[0])
+        else:
+            x0 = ceil(x[0]) + 1
+        return x1 - x0 + 1
+
+
+def quadratic_solver(a: float, b: float, c: float) -> list:
+    x1 = (-1 * b + sqrt(b**2 - 4 * a * c)) / (2 * a)
+    x2 = (-1 * b - sqrt(b**2 - 4 * a * c)) / (2 * a)
+    res = [x1, x2]
+    res.sort()
+    return res
+
+
+def parse_input(file_path: Path = Path("./input/input.txt")) -> list[Race]:
+    # regex to find all digits in a line
+    pattern = re.compile("\d+")
     # First make grid
     with open(file_path, "r") as file:
-        full_text = file.read()
-
-    return None
+        # get times
+        times = pattern.findall(file.readline())
+        # get distances
+        distances = pattern.findall(file.readline())
+        # organize into races
+        races = [
+            Race(time=int(t), record_distance=int(d)) for t, d in zip(times, distances)
+        ]
+    return races
--- days/day_6/challenge_1/main.py
+++ days/day_6/challenge_1/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from common.utils import parse_input
 import logging
 from rich.logging import RichHandler
+from functools import reduce
 
 logger = logging.getLogger(__name__)
 
@@ -10,5 +11,8 @@ if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG, format="%(message)s", handlers=[RichHandler()]
     )
-    # input_path = Path("./days/day_5/challenge_1/input/input.txt")
-    # parse_input(input_path)
+    input_path = Path("./days/day_6/challenge_1/input/input.txt")
+    races = parse_input(input_path)
+    # product all records
+    res = reduce(lambda x, y: x * y, [r.beat_record_count for r in races], 1.0)
+    logger.info(f"****Answer: {res}****")
